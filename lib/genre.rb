3ee7f937865f8633fddc802273cc1289f9e32ab2
Chovin Carlson <Chovin@users.noreply.github.com>
2019-02-19 23:28:24 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,17 @@
 class Genre
+  attr_reader :name, :genre, :artist
+  @@all = []
   
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    
+  end
 end
